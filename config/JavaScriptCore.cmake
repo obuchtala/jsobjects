190d70b4a438dc45f7ecf896d1de1fec3e65d20a
Oliver Buchtala <oliver@Olivers-Mac-mini.local>
2012-11-19 22:44:39 +0100

    Configure JSC library flags for OSX

--- config/JavaScriptCore.cmake
+++ config/JavaScriptCore.cmake
@@ -1,10 +1,11 @@
 
 if(APPLE)
-# nothing to be done
+
+    find_library(JSC JavaScriptCore)
 
 elseif(UNIX)
-  FIND_PATH(JSC_INCLUDE_DIR "JavaScriptCore/JavaScriptCore.h" "/usr/include/webkitgtk-1.0")
-  SET(JSC_LIBS javascriptcoregtk-1.0)
-  SET(JSC_INCLUDE_DIRS ${JSC_INCLUDE_DIR})
+    FIND_PATH(JSC_INCLUDE_DIR "JavaScriptCore/JavaScriptCore.h" "/usr/include/webkitgtk-1.0")
+    SET(JSC_LIBRARIES javascriptcoregtk-1.0)
+    SET(JSC_INCLUDE_DIRS ${JSC_INCLUDE_DIR})
 
 endif()
